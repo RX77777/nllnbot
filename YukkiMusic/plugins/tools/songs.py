7a0765836c42b8b89d26d39b7ec042df5ff6543e
Ahmad Qa <113384114+YaFaMusic@users.noreply.github.com>
2022-09-15 02:22:04 +0300

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -30,7 +30,8 @@ SONG_COMMAND = get_command("SONG_COMMAND")
 
 
 @app.on_message(
-    filters.command(SONG_COMMAND)    
+    filters.command(SONG_COMMAND)   
+    & filters.group
     & ~filters.edited
     & ~BANNED_USERS
 )
@@ -54,7 +55,7 @@ async def song_commad_group(client, message: Message, _):
 
 @app.on_message(
     filters.command(SONG_COMMAND)
-    & filters.group
+    & filters.private
     & ~filters.edited
     & ~BANNED_USERS
 )
