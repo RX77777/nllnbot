a9b2e147a19e6c95fa327e28ee718277c31f70cd
Ahmad Qa <113384114+YaFaMusic@users.noreply.github.com>
2022-09-16 01:12:03 +0300

    Update yafa_joun.py

--- YukkiMusic/plugins/tools/yafa_joun.py
+++ YukkiMusic/plugins/tools/yafa_joun.py
@@ -1,11 +1,12 @@
 from pyrogram import Client, filters
+from strings.filters import command
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from config import YAFA_CHANNEL, YAFA_NAME, CHANNEL_SUDO
 from YukkiMusic import app
 
 
-@app.on_message(command(["start","play","mute","skip","stop","song","cmute","sudolist",
+@app.on_message(filters.command(["start","play","mute","skip","stop","song","cmute","sudolist",
                          "cplay","resume","vplay","channelplay","stats"]))
 async def must_join_channel(bot: Client, msg: Message):
     if not YAFA_CHANNEL:  # Not compulsory
