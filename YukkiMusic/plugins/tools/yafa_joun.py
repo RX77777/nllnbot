ecbaa3c3a73baef9cf3874140170d8543441ce98
Ahmad Qa <113384114+YaFaMusic@users.noreply.github.com>
2022-09-16 01:12:47 +0300

    Update yafa_joun.py

--- YukkiMusic/plugins/tools/yafa_joun.py
+++ YukkiMusic/plugins/tools/yafa_joun.py
@@ -7,7 +7,7 @@ from YukkiMusic import app
 
 
 @app.on_message(filters.command(["start","play","mute","skip","stop","song","cmute","sudolist",
-                         "cplay","resume","vplay","channelplay","stats"]))
+                         "cplay","resume","vplay","channelplay","stats","شغل","تشغيل"]))
 async def must_join_channel(bot: Client, msg: Message):
     if not YAFA_CHANNEL:  # Not compulsory
         return
