b3f08b6ef3e9d02da1194cb2a7156944b2b53d19
YafaMuwicQ <113455680+YafaMuwicQ@users.noreply.github.com>
2022-09-17 23:16:50 +0300

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -69,7 +69,7 @@ async def helper_private(
         _ = get_string(language)
         keyboard = help_pannel(_)
         await message.reply_photo(
-         photo=config.START_IMG_URL
+         photo=config.START_IMG_URL,
         caption=_["help_1"], reply_markup=keyboard))
 
 
@@ -83,7 +83,7 @@ async def helper_private(
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
     await message.reply_photo(
-     photo=config.START_IMG_URL
+     photo=config.START_IMG_URL,
     caption=_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard))
     )
 
