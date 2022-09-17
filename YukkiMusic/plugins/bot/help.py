f6b4cc7a6347597c4a67a6782be7c478384eae66
YafaMuwicQ <113455680+YafaMuwicQ@users.noreply.github.com>
2022-09-17 23:04:41 +0300

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -68,8 +68,8 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await apdate.reply_photo(
-         photo = config.START_IMG_URL
+        await message.reply_photo(
+         photo=config.START_IMG_URL
         await update.reply_text(_["help_1"], reply_markup=keyboard))
 
 
@@ -82,6 +82,8 @@ async def helper_private(
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
+    await message.reply_photo(
+     photo=config.START_IMG_URL
     await message.reply_text(
         _["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
     )
@@ -96,7 +98,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb5":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "Only for Sudo Users", show_alert=True
+                "هذا الأمر فقط للمطورين", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
