1d28c830464f04d5bb66d5f0b8d2cbcc3847822a
YafaMuwicQ <113455680+YafaMuwicQ@users.noreply.github.com>
2022-09-17 20:28:39 +0300

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -50,8 +50,7 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_, True)
-        if update.message.photo:
-            await update.message.delete()
+        if update.message.photo:            
             await update.message.reply_text(
                 _["help_1"], reply_markup=keyboard
             )
