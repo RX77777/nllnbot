7b45ffddc406b5bc1f35ff7f0e39c78fa47852a3
YafaMuwicQ <113455680+YafaMuwicQ@users.noreply.github.com>
2022-09-18 04:28:09 +0300

    Update help.py

--- YukkiMusic/utils/inline/help.py
+++ YukkiMusic/utils/inline/help.py
@@ -53,7 +53,7 @@ def help_pannel(_, START: Union[bool, int] = None):
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_6"],
+                    text="اوامر المطورين,
                     callback_data="help_callback hb5",
                 ),
             ],
