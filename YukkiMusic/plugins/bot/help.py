90a490dd7151608b3197c3b7b86320c21eacad36
YafaMuwicQ <113455680+YafaMuwicQ@users.noreply.github.com>
2022-09-19 02:39:37 +0300

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -124,7 +124,7 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(
             helpers.HELP_4, reply_markup=keyboard
         )
-   elif cb == "hb6":
+   elif cb == "hb7":
         await CallbackQuery.edit_message_text(
             helpers.HELP_6, reply_markup=keyboard
         )
