db26489db22c043a089a4df3ac82d87d47955b50
RX77777 <121124279+RX77777@users.noreply.github.com>
2024-02-26 06:13:54 +0300

    Update mongo.py

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -15,7 +15,7 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority"
 
 
 if config.MONGO_DB_URI is None:
